893d1857107306d674412dd7a19e7e32bf601de2
jamisuomalainenn <Jami.Suomalainen@student.lab.fi>
2024-02-10 14:54:50 +0100

    10.2. toinen commit

--- lib/views/dashboard.dart
+++ lib/views/dashboard.dart
@@ -4,15 +4,18 @@ class NewsDashboard extends StatelessWidget {
   final List<Map<String, String>> newsArticles = [
     {
       'title': 'Breaking News: Flutter 3.0 Released',
-      'description': 'Flutter 3.0 comes with significant updates, including null safety features, web support improvements, and more.',
+      'description':
+          'Flutter 3.0 comes with significant updates, including null safety features, web support improvements, and more.',
     },
     {
       'title': 'Tech Update: AI Revolutionizing Industries',
-      'description': 'From healthcare to finance, Artificial Intelligence is transforming every sector with its innovative solutions.',
+      'description':
+          'From healthcare to finance, Artificial Intelligence is transforming every sector with its innovative solutions.',
     },
     {
       'title': 'Global Event: Climate Change Conference 2023',
-      'description': 'Experts from around the world gather to discuss climate change impacts and sustainable solutions for the future.',
+      'description':
+          'Experts from around the world gather to discuss climate change impacts and sustainable solutions for the future.',
     },
     // Add more mock news articles here
   ];
@@ -21,7 +24,8 @@ class NewsDashboard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('moi'),
+        title: Text('Top News'),
+        centerTitle: true,
       ),
       body: ListView.builder(
         itemCount: newsArticles.length,
@@ -34,11 +38,35 @@ class NewsDashboard extends StatelessWidget {
               subtitle: Text(article['description']!),
               onTap: () {
                 // Handle the tap event, e.g., navigate to a detailed news view
-                print('Tapped on ${article['title']}');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => NewsDetails(article: article),
+                  ),
+                );
               },
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+class NewsDetails extends StatelessWidget {
+  final Map<String, String> article;
+
+  const NewsDetails({required this.article});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(article['title']!),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text(article['description']!),
       ),
     );
   }
