e1ad0a88d7466ba648611ecf0c18e649c7a38da3
jamisuomalainenn <Jami.Suomalainen@student.lab.fi>
2024-02-10 14:34:05 +0100

    First Push /jami

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:news_app/firebase_options.dart';
-import  'package:news_app/views/dashboard.dart';
+import 'package:news_app/views/dashboard.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,7 +11,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp (const MainApp());
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
@@ -21,22 +21,21 @@ class MainApp extends StatelessWidget {
     final providers = [EmailAuthProvider()];
 
     return MaterialApp(
-      initialRoute: FirebaseAuth.instance.currentUser == null ? '/sign-in' : '/profile',
+      initialRoute:
+          FirebaseAuth.instance.currentUser == null ? '/sign-in' : '/dashboard',
       routes: {
         '/sign-in': (context) {
           return SignInScreen(
             providers: providers,
             actions: [
               AuthStateChangeAction<SignedIn>((context, state) {
-                Navigator.pushReplacementNamed(context, '/profile');
+                Navigator.pushReplacementNamed(context, '/dashboard');
               }),
             ],
           );
         },
-        '/profile': (context) {
-          return NewsDashboard(
-           
-          );
+        '/dashboard': (context) {
+          return NewsDashboard();
         },
       },
     );
